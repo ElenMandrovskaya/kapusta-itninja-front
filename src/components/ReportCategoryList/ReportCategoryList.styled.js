27b7ba7d37bc17f26aee90c53437322c86c6f7bf
Svetlana Lazareva <lazarevas3488@gmail.com>
2021-11-08 02:34:15 +0200

    add components

---
+++ src/components/ReportCategoryList/ReportCategoryList.styled.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const CategoryList = styled.ul`
+  margin: -5px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media screen and (min-width: 768px) {
+    padding: 20px;
+    margin-bottom: 30px;
+
+    background-color: #ffffff;
+    box-shadow: 0px 10px 60px rgba(170, 178, 197, 0.2);
+    border-radius: 30px;
+  }
+  @media screen and (min-width: 1280px) {
+    padding: 20px 220px 30px 225px;
+  }
+`;
