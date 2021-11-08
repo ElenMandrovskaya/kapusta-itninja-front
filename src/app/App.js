31c89af347b9cc05636c3961d91519819218b5a3
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-11-08 16:11:06 +0200

    Header

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from './App.styled';
 import { Test } from '../components/Test/Test';
+import  { Header } from "../components/Header/Header";
 
 export default function App() {
     return (
     <Container>
+       <Header /> 
       <Test/>
-    </Container>
+     </Container>
   )
 }
---
+++ src/components/UserLogout/UserLogout.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { ReactComponent as LogoutSvg } from '../../images/logout.svg';
+// import {  Button, Text } from "./UserLogout.styled";
+
+
+// const UserLogout = () => {
+//     return (
+   
+//         <Button>
+//           <LogoutSvg />
+//              <Text>Выйти</Text>
+//         <Button />
+    
+//     );
+//   };
+  
+//   export default UserLogout;
