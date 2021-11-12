3c67d99e7b06ac1240a1351ff70763ce079f2db7
Ekaterina Vereshchagina <vereshchaginaea@icloud.com>
2021-11-12 16:48:32 +0200

    page

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BgGrey, Container } from './App.styled';
-import { Header } from '../components/Header/Header';
-import { Balance } from '../components/Balance/Balance';
+// import { Header } from '../components/Header/Header';
+// import { Balance } from '../components/Balance/Balance';
 import StatementBtn from '../components/GoToStatementsButton/GoToStatementsButton.jsx';
 import GoBackHomeBtn from '../components/GoBackHomeButton/GoBackHomeButton';
 import MonthPicker from '../components/MonthPicker/MonthPicker';
@@ -9,8 +9,8 @@ import StatisticAmounts from '../components/StatisticAmounts/StatisticAmounts.js
 import MyChart from '../components/Charts/Charts';
 import TransactionPage from "../pages/TransactionsPage/TransactionsPage"
 import {Report} from '../components/Report/Report';
-import { BgUnAuth } from '../components/BgUnAuth/BgUnAuth';
-import { BgAuth } from '../components/BgAuth//BgAuth';
+// import { BgUnAuth } from '../components/BgUnAuth/BgUnAuth';
+// import { BgAuth } from '../components/BgAuth//BgAuth';
 import RegistrationPage from '../pages/RegistrationPage';
 // import { LogoutModal } from '../components/LogoutModal/LogoutModal'
 
@@ -25,7 +25,7 @@ export default function App() {
       {/* <BgUnAuth />   */}
       <Container>
         {/* <HeroTitle /> */}
-        <Balance /> 
+        {/* <Balance />  */}
         <TransactionPage />
         <GoBackHomeBtn />
         <StatementBtn />
--- src/pages/TransactionsPage/TransactionsPage.js
+++ src/pages/TransactionsPage/TransactionsPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Balance from "../../components/Balance/Balance";
+import GoToStatementsButton from "../../components/GoToStatementsButton/GoToStatementsButton.jsx"
 import TransactionsExpForm from "../../components/TransactionsExpForm/TransactionsExpForm";
 import TransactionsExpense from "../../components/TransactionsExpense/TransactionsExpense";
 import TransactionsIncome from "../../components/TransactionsIncome/TransactionsIncome";
 import TransactionsIncForm from "../../components/TransactionsIncForm/TransactionsIncForm";
+import { Container } from "../../app/App.styled";
 
 const TransactionsPage = () => {
     return (
+        <Container>
+            <Balance />
+            <GoToStatementsButton />
             <div>
                 <Tabs>
                     <TabList>
@@ -23,6 +29,7 @@ const TransactionsPage = () => {
                     </TabPanel>
                 </Tabs>
             </div> 
+        </Container>
     );
 };
 
