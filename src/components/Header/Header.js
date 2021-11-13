b0ac4cecee52c852c394b6e622357df95d49a623
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-11-13 04:26:32 +0200

    кнопка выйти

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as authOperations from '../../redux/auth/auth-operations';
 import { authSelectors } from '../../redux/auth/auth-selectors'
-import LogoutModal from '../LogoutModal/LogoutModal';
+// import LogoutModal from '../LogoutModal/LogoutModal';
 import { ContainerHeader, UserInfo, UserName, Button, LogoutP, AvatarUser, AvatarTxt, Line, LogoutSvg , Logo } from "./Header.styled";
 import defaultAvatar from '../../images/avatar.png';
 
 
 export function Header () {
     const dispatch = useDispatch();
-    const name = useSelector(authSelectors.getUserName);
+    // const name = useSelector(authSelectors.getUserName);
     const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
     return (
@@ -37,7 +37,7 @@ export function Header () {
                     <LogoutSvg />
                 </Button>
 
-               {isModalLogout && <LogoutModal />}
+               {/* {isModalLogout && <LogoutModal />} */}
             </UserInfo>
              )}
         </ContainerHeader>      
