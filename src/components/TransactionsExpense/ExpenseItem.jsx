21ee7988454a99694cf0310588f589fab5a9cf29
Ekaterina Vereshchagina <vereshchaginaea@icloud.com>
2021-11-13 19:41:22 +0200

    Update ExpenseItem.jsx

--- src/components/TransactionsExpense/ExpenseItem.jsx
+++ src/components/TransactionsExpense/ExpenseItem.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import useModal from "../Modal/useModal";
+import Modal from "../Modal/logoutModal";
 import { Item, ItemDate, ItemDesc, ItemCategory, ItemSum, ItemBtn } from "./ExpenseItem.styled"
 
-const ExpenseItem = () => {
+function ExpenseItem(
+    // { date, description, amount, id, category }
+) {
+    // const dispatch = useDispatch();
+    const { isShowingModal, toggle } = useModal();
+    
     return (
         <Item>
-            <ItemDate>дата</ItemDate>
-            <ItemDesc>описание</ItemDesc>
-            <ItemCategory>категория</ItemCategory>
-            <ItemSum> -  грн.</ItemSum>
-            <ItemBtn type="button" />
+            <ItemDate>date</ItemDate>
+            <ItemDesc>description</ItemDesc>
+            <ItemCategory>category</ItemCategory>
+            <ItemSum>-amount грн.</ItemSum>
+            <ItemBtn
+                type="button"
+                onClick={()=>toggle()}
+            />
+            {isShowingModal && <Modal text={'Вы уверены?'} onClose={toggle} />}
         </Item>
     );
 };
