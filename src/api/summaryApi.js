e4aa0f5f67253f17fde6fe618eb1a18fdde0f81d
Elena <e.mandrovska@gmail.com>
2021-11-14 10:54:16 +0200

    summary api for transactionpage

---
+++ src/api/summaryApi.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+// РЕПОРТЫ ДЛЯ СВОДКИ (В ТРАЗАКЦАКШН ПЕЙДЖ)
+// пример запроса РАСХОД GET
+// https://kapusta-finance-tracker.herokuapp.com/api/reports/expense?year=2021
+
+export async function getSummaryExpense() {
+    const {data} = await axios.get(`/api/reports/expense?year=2021`);
+    console.log(data.result)
+    return data.result;
+};
+// ответ
+// {
+//     "status": "success",
+//     "code": 200,
+//     "result": {
+//         "finalReportArray": [
+//             {
+//                 "month": "11",
+//                 "value": 15000
+//             }
+//         ]
+//     }
+// }
+
+
+// пример запроса ДОХОД GET
+// https://kapusta-finance-tracker.herokuapp.com/api/reports/income?year=2021
+
+export async function getSummaryIncome() {
+    const {data} = await axios.get(`/api/reports/expense?year=2021`);
+    console.log(data.result)
+    return data.result;
+};
+// ответ 
+// {
+//     "status": "success",
+//     "code": 200,
+//     "result": {
+//         "finalReportArray": [
+//             {
+//                 "month": "11",
+//                 "value": 50000
+//             }
+//         ]
+//     }
+// }
