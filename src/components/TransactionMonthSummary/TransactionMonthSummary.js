6e6372d9bf6f332de6273c126a9f94acb59f8394
Elena <e.mandrovska@gmail.com>
2021-11-14 15:00:05 +0200

    summary

--- src/components/TransactionMonthSummary/TransactionMonthSummary.js
+++ src/components/TransactionMonthSummary/TransactionMonthSummary.js
@@ -1,33 +1,32 @@
 import React, {useState, useEffect} from "react";
 import { Summary, Title, SummaryList, SummaryItem  } from "./TransactionMonthSummary.styled"
 import {getSummaryExpense, getSummaryIncome} from "../../api/summaryApi"
-import getStoredState from "redux-persist/es/getStoredState";
 
 const TransactionMonthSummary = () => {
-    // const expenseSummary = getSummaryExpense();
-//     const [month, setMonth] = useState("");
-//     const [amount, setAmount] = useState("");
-//     useEffect(() => {
-//         async function SumExp() {
-//     try {
-//         const data = await getSummaryExpense();
-//         setMonth
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//     SumExp()
-// })
-//     console.log()
+    const [summary, setSummary] = useState("");
+
+    useEffect(() => {
+        async function SumExp() {
+    try {
+        const summary = await getSummaryExpense();
+        setSummary(summary)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    SumExp()
+}, [])
+console.log(summary)
     return (
         <Summary>
             <Title>Сводка</Title>
+            {summary && summary.map(({month, value}) =>
             <SummaryList>
                     <SummaryItem >
                         <span>{month}</span>
-                        <span>{amount}</span>
+                        <span>{value}</span>
                     </SummaryItem>                
-            </SummaryList>
+            </SummaryList>)}
         </Summary>
     );
 };
