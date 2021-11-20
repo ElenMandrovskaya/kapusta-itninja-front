576c14172a90f2995b1d53b4c5b0ffa2c3b5f457
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-11-20 07:27:33 +0200

    add logic for GoogleAuth

--- src/app/App.js
+++ src/app/App.js
@@ -1,66 +1,73 @@
-import React, { useEffect, Suspense, lazy } from "react";
-import { Switch } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { ToastContainer } from "react-toastify";
+import React, { useEffect, Suspense, lazy } from 'react';
+import { Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 // import TransactionPage from "../pages/TransactionsPage/TransactionsPage";
 // import RegistrationPage from "../pages/RegistrationPage";
 // import ReportsPage from "../pages/ReportsPage/ReportsPage";
 
-import PrivateRoute from "../routers/PrivateRouter";
-import PublicRoute from "../routers/PublicRouter";
+import PrivateRoute from '../routers/PrivateRouter';
+import PublicRoute from '../routers/PublicRouter';
 
-import * as authOperations from "../redux/auth/auth-operations";
-import { authSelectors } from "../redux/auth/auth-selectors";
-import { BgGrey, AppWrap, Container } from "./App.styled";
-import BgUnAuth from "../components/BgUnAuth/BgUnAuth";
-import BgAuth from "../components/BgAuth/BgAuth";
-import Header from "../components/Header/Header";
-import Spinner from "../components/Spinner/Spinner";
+import * as authOperations from '../redux/auth/auth-operations';
+import { authSelectors } from '../redux/auth/auth-selectors';
+import { BgGrey, AppWrap, Container } from './App.styled';
+import BgUnAuth from '../components/BgUnAuth/BgUnAuth';
+import BgAuth from '../components/BgAuth/BgAuth';
+import Header from '../components/Header/Header';
+import Spinner from '../components/Spinner/Spinner';
 
 const RegistrationPage = lazy(() => import('../pages/RegistrationPage'));
 const TransactionPage = lazy(() => import('../pages/TransactionsPage/TransactionsPage'));
 const ReportsPage = lazy(() => import('../pages/ReportsPage/ReportsPage'));
 const ExpensesFormPage = lazy(() => import('../pages/ExpensesIncomeFormPage/ExpensesFormPage'));
 const IncomesFormPage = lazy(() => import('../pages/ExpensesIncomeFormPage/IncomesFormPage'));
+const GoogleRedirect = lazy(() => import('../pages/GoogleRedirect/GoogleRedirect'));
 
 export default function App() {
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(authOperations.getCurrentUser());
-  }, [dispatch]);
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+    const dispatch = useDispatch();
 
-  return (
-    <AppWrap>
-      <BgGrey />
-      {isLoggedIn ? <BgAuth /> : <BgUnAuth />}
-      <Header />
-      <Container>
-      <Suspense fallback={<Spinner/>}>
-        <Switch>
-          <PublicRoute exact path="/" edirectTo="/transactions" restricted>
-            <RegistrationPage />
-          </PublicRoute>
-          <PrivateRoute exact path="/transactions">
-            <TransactionPage />
-          </PrivateRoute>
-          <PrivateRoute path="/reports">
-            <ReportsPage />
-          </PrivateRoute>
-          <PrivateRoute path="/transactions_expenses_form">
-            <ExpensesFormPage />
-          </PrivateRoute>
-          <PrivateRoute path="/transactions_incomes_form">
-            <IncomesFormPage />
-          </PrivateRoute>
-        </Switch>
-        </Suspense>
-      </Container>
-      <ToastContainer />
-    </AppWrap>
-  );
+    useEffect(() => {
+        dispatch(authOperations.getCurrentUser());
+    }, [dispatch]);
+    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+    return (
+        <AppWrap>
+            <BgGrey />
+            {isLoggedIn ? <BgAuth /> : <BgUnAuth />}
+            <Header />
+            <Container>
+                <Suspense fallback={<Spinner />}>
+                    <Switch>
+                        <PublicRoute
+                            exact
+                            path="/google-redirect"
+                            redirectTo="/transactions"
+                            restricted
+                        >
+                            <GoogleRedirect />
+                        </PublicRoute>
+                        <PublicRoute exact path="/" redirectTo="/transactions" restricted>
+                            <RegistrationPage />
+                        </PublicRoute>
+                        <PrivateRoute exact path="/transactions">
+                            <TransactionPage />
+                        </PrivateRoute>
+                        <PrivateRoute path="/reports">
+                            <ReportsPage />
+                        </PrivateRoute>
+                        <PrivateRoute path="/transactions_expenses_form">
+                            <ExpensesFormPage />
+                        </PrivateRoute>
+                        <PrivateRoute path="/transactions_incomes_form">
+                            <IncomesFormPage />
+                        </PrivateRoute>
+                    </Switch>
+                </Suspense>
+            </Container>
+            <ToastContainer />
+        </AppWrap>
+    );
 }
-
-
--- src/components/SignInForm/authForm.js
+++ src/components/SignInForm/authForm.js
@@ -1,173 +1,177 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 // import { FcGoogle } from "react-icons/fc";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from "react-redux";
-import * as authOperations from "../../redux/auth/auth-operations";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch } from 'react-redux';
+import * as authOperations from '../../redux/auth/auth-operations';
 // import { authSelectors } from "../../redux/auth/auth-selectors";
-import GoogleAuth from "../GoogleAuth/GoogleAuth";
+import GoogleAuth from '../GoogleAuth/GoogleAuth';
 // import * as Yup from "yup";
+import { SpanTextWrapper, AuthGoogleBtn } from './AuthForm.styled';
+import { FcGoogle } from 'react-icons/fc';
 
 import {
-  AuthGoogleDescription,
-  OtherDescriptionToSignUp,
-  SignInFormWrapper,
-  LabelInputForm,
-  FormInputDescription,
-  FormInput,
-  FormBtn,
-} from "./AuthForm.styled.js";
+    AuthGoogleDescription,
+    OtherDescriptionToSignUp,
+    SignInFormWrapper,
+    LabelInputForm,
+    FormInputDescription,
+    FormInput,
+    FormBtn,
+} from './AuthForm.styled.js';
 
 export default function AuthForm() {
-  const dispatch = useDispatch();
-  const [isRegistration, setRegistration] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const [isRegistration, setRegistration] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  // let schemaMail = Yup.string().email();
-  // let result = schemaMail.isValidSync("test@test.com"); // isValidSync returns boolean
-  // console.log(result);
+    // let schemaMail = Yup.string().email();
+    // let result = schemaMail.isValidSync("test@test.com"); // isValidSync returns boolean
+    // console.log(result);
 
-  const handleChange = e => {
-    const { name, value } = e.currentTarget;
-    switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "email":
-        setEmail(value);
-        break;
-      case "password":
-        setPassword(value);
-        break;
+    const handleChange = e => {
+        const { name, value } = e.currentTarget;
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
 
-      default:
-        return;
-    }
-  };
+            default:
+                return;
+        }
+    };
 
-  const handleChangeForm = e => {
-    e.preventDefault();
-    setRegistration(!isRegistration);
-  };
+    const handleChangeForm = e => {
+        e.preventDefault();
+        setRegistration(!isRegistration);
+    };
 
-  const resetInputs = () => {
-    setEmail("");
-    setPassword("");
-    setName("");
-  };
+    const resetInputs = () => {
+        setEmail('');
+        setPassword('');
+        setName('');
+    };
 
-  const OnSubmitRegBtn = evt => {
-    evt.preventDefault();
-    if (!name || !email || !password) {
-      toast.info("Fill in all the fields")
-      return;
-    }
-    dispatch(authOperations.signUp({ name, email, password }));
-    resetInputs();
-  };
+    const OnSubmitRegBtn = evt => {
+        evt.preventDefault();
+        if (!name || !email || !password) {
+            toast.info('Fill in all the fields');
+            return;
+        }
+        dispatch(authOperations.signUp({ name, email, password }));
+        resetInputs();
+    };
 
-  const OnSubmitSignInBtn = evt => {
-    evt.preventDefault();
-    if (!email || !password) {
-      toast.info("Fill in all the fields")
-      return;
-    }
-    dispatch(authOperations.signIn({ email, password }));
-    resetInputs();
-  };
+    const OnSubmitSignInBtn = evt => {
+        evt.preventDefault();
+        if (!email || !password) {
+            toast.info('Fill in all the fields');
+            return;
+        }
+        dispatch(authOperations.signIn({ email, password }));
+        resetInputs();
+    };
 
-  return (
-    <Fragment>
-      {/* <AuthGoogleBtn type="button">
+    return (
+        <Fragment>
+            {/* <AuthGoogleBtn type="button">
         <FcGoogle size={18} />
         <SpanTextWrapper>Google</SpanTextWrapper>
       </AuthGoogleBtn> */}
-      {isRegistration ? null : (
-        <Fragment>
-          <AuthGoogleDescription>
-            Вы можете авторизоваться с помощью Google Account:
-          </AuthGoogleDescription>
-          <GoogleAuth />
-        </Fragment>
-      )}
+            {isRegistration ? null : (
+                <Fragment>
+                    <AuthGoogleDescription>
+                        Вы можете авторизоваться с помощью Google Account:
+                    </AuthGoogleDescription>
+                    <a
+                        href="https://kapusta-finance-tracker.herokuapp.com/api/user/google"
+                        alt="GoogleAuth"
+                    >
+                        <AuthGoogleBtn type="button">
+                            <FcGoogle size={18} />
+                            <SpanTextWrapper>Google</SpanTextWrapper>
+                        </AuthGoogleBtn>
+                    </a>
+                    {/* <GoogleAuth /> */}
+                </Fragment>
+            )}
 
-      <OtherDescriptionToSignUp>
-        Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
-      </OtherDescriptionToSignUp>
-      <SignInFormWrapper
-        //   onSubmit={handleSubmit}
-        action=""
-        autoComplete="on"
-      >
-        {isRegistration ? (
-          <LabelInputForm>
-            <FormInputDescription>Введите имя:</FormInputDescription>
-            <FormInput
-              //   onBlur={blurHandler}
-              placeholder={"Barry Donatello"}
-              type="name"
-              name="name"
-              onChange={handleChange}
-              value={name}
-              required
-            />
-          </LabelInputForm>
-        ) : null}
-
-        <LabelInputForm>
-          <FormInputDescription>Электронная почта:</FormInputDescription>
-          <FormInput
-            // onBlur={blurHandler}
-            placeholder={"your@email.com"}
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={email}
-            required
-          />
-        </LabelInputForm>
-
-        <LabelInputForm marginBTM>
-          <FormInputDescription>Пароль:</FormInputDescription>
-          <FormInput
-            placeholder={"········"}
-            type="password"
-            name="password"
-            onChange={handleChange}
-            value={password}
-            required
-          />
-        </LabelInputForm>
-
-        {isRegistration ? (
-          <Fragment>
-            <FormBtn type="button" marginRigth15 onClick={handleChangeForm}>
-              Войти
-            </FormBtn>
-            <FormBtn type="submit" submitBtn onClick={OnSubmitRegBtn}>
-              Регистрация
-            </FormBtn>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <FormBtn
-              type="submit"
-              marginRigth15
-              submitBtn
-              onClick={OnSubmitSignInBtn}
+            <OtherDescriptionToSignUp>
+                Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
+            </OtherDescriptionToSignUp>
+            <SignInFormWrapper
+                //   onSubmit={handleSubmit}
+                action=""
+                autoComplete="on"
             >
-              Войти
-            </FormBtn>
-            <FormBtn type="button" onClick={handleChangeForm}>
-              Регистрация
-            </FormBtn>
-          </Fragment>
-        )}
-      </SignInFormWrapper>
-    </Fragment>
-  );
-}
+                {isRegistration ? (
+                    <LabelInputForm>
+                        <FormInputDescription>Введите имя:</FormInputDescription>
+                        <FormInput
+                            //   onBlur={blurHandler}
+                            placeholder={'Barry Donatello'}
+                            type="name"
+                            name="name"
+                            onChange={handleChange}
+                            value={name}
+                            required
+                        />
+                    </LabelInputForm>
+                ) : null}
 
- 
+                <LabelInputForm>
+                    <FormInputDescription>Электронная почта:</FormInputDescription>
+                    <FormInput
+                        // onBlur={blurHandler}
+                        placeholder={'your@email.com'}
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        value={email}
+                        required
+                    />
+                </LabelInputForm>
+
+                <LabelInputForm marginBTM>
+                    <FormInputDescription>Пароль:</FormInputDescription>
+                    <FormInput
+                        placeholder={'········'}
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        value={password}
+                        required
+                    />
+                </LabelInputForm>
+
+                {isRegistration ? (
+                    <Fragment>
+                        <FormBtn type="button" marginRigth15 onClick={handleChangeForm}>
+                            Войти
+                        </FormBtn>
+                        <FormBtn type="submit" submitBtn onClick={OnSubmitRegBtn}>
+                            Регистрация
+                        </FormBtn>
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <FormBtn type="submit" marginRigth15 submitBtn onClick={OnSubmitSignInBtn}>
+                            Войти
+                        </FormBtn>
+                        <FormBtn type="button" onClick={handleChangeForm}>
+                            Регистрация
+                        </FormBtn>
+                    </Fragment>
+                )}
+            </SignInFormWrapper>
+        </Fragment>
+    );
+}
