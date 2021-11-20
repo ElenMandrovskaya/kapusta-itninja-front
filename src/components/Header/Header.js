5438f07655ec63c3f2170e3583351522e5765573
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-11-20 21:03:42 +0200

    Додав перевірку на помилки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,59 +1,60 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import * as authOperations from "../../redux/auth/auth-operations";
-import { authSelectors } from "../../redux/auth/auth-selectors";
-import Modal from "../Modal/logoutModal";
-import useModal from "../Modal/useModal";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as authOperations from '../../redux/auth/auth-operations';
+import { authSelectors } from '../../redux/auth/auth-selectors';
+import Modal from '../Modal/logoutModal';
+import useModal from '../Modal/useModal';
 import {
-  ContainerHeader,
-  UserInfo,
-  UserName,
-  Button,
-  LogoutP,
-  AvatarUser,
-  Line,
-  LogoutSvg,
-  Logo,
-} from "./Header.styled";
-import defaultAvatar from "../../images/avatar.png";
+    ContainerHeader,
+    UserInfo,
+    UserName,
+    Button,
+    LogoutP,
+    AvatarUser,
+    Line,
+    LogoutSvg,
+    Logo,
+} from './Header.styled';
+import defaultAvatar from '../../images/avatar.png';
 
 export default function Header() {
-  const dispatch = useDispatch();
-  const name = useSelector(authSelectors.getUserName);
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  const logout = () => dispatch(authOperations.signOut());
-  const { isShowingModal, toggle } = useModal();
-  return (
-    <ContainerHeader>
-      <Logo />
-      {isLoggedIn && (
-        <UserInfo>
-          <AvatarUser>
-            <img src={defaultAvatar} alt="Default Avatar" width="32" />
-            {/* <AvatarTxt>U</AvatarTxt> */}
-          </AvatarUser>
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+    const name = useSelector(authSelectors.getUserName) || '';
 
-          {/* <UserName>User Name</UserName> */}
-          <UserName>{name}</UserName>
-          <Line> </Line>
+    const logout = () => dispatch(authOperations.signOut());
+    const { isShowingModal, toggle } = useModal();
+    return (
+        <ContainerHeader>
+            <Logo />
+            {isLoggedIn && (
+                <UserInfo>
+                    <AvatarUser>
+                        <img src={defaultAvatar} alt="Default Avatar" width="32" />
+                        {/* <AvatarTxt>U</AvatarTxt> */}
+                    </AvatarUser>
 
-          <Button
-            type="button"
-            // onClick={() => dispatch(authOperations.signOut())}
-            onClick={() => toggle()}
-          >
-            <LogoutP>Выйти</LogoutP>
-            <LogoutSvg />
-          </Button>
-          {isShowingModal && (
-            <Modal
-              text={"Вы уверены, что хотите выйти?"}
-              toAgree={logout}
-              onClose={toggle}
-            />
-          )}
-        </UserInfo>
-      )}
-    </ContainerHeader>
-  );
+                    {/* <UserName>User Name</UserName> */}
+                    <UserName>{name}</UserName>
+                    <Line> </Line>
+
+                    <Button
+                        type="button"
+                        // onClick={() => dispatch(authOperations.signOut())}
+                        onClick={() => toggle()}
+                    >
+                        <LogoutP>Выйти</LogoutP>
+                        <LogoutSvg />
+                    </Button>
+                    {isShowingModal && (
+                        <Modal
+                            text={'Вы уверены, что хотите выйти?'}
+                            toAgree={logout}
+                            onClose={toggle}
+                        />
+                    )}
+                </UserInfo>
+            )}
+        </ContainerHeader>
+    );
 }
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,17 +1,17 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
-const getUserName = state => state.auth.user.name;
+const getUserName = state => state.auth.user?.name;
 
-const getUserEmail = state => state.auth.user.email;
+const getUserEmail = state => state.auth.user?.email;
 
-const getCurrentBalance = state => state.auth.user.balance;
+const getCurrentBalance = state => state.auth.user?.balance;
 
 // const getUserAvatar = state => state.auth.user.avatar;
 
 export const authSelectors = {
     getIsLoggedIn,
     getUserName,
-    getUserEmail, 
-    getCurrentBalance
+    getUserEmail,
+    getCurrentBalance,
     // getUserAvatar
-}
+};
